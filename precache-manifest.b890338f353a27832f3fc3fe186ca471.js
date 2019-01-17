d57d3a9c5bdf761473a73991a1a6dfab05eefda0
Michael Parisi <mikeparisi@gmail.com>
2019-01-16 16:42:31 -0800

    Updates

---
+++ precache-manifest.b890338f353a27832f3fc3fe186ca471.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6943cea7f431d5d563c",
+    "url": "/ring-builder-react/static/css/main.bdc5e19e.chunk.css"
+  },
+  {
+    "revision": "e6943cea7f431d5d563c",
+    "url": "/ring-builder-react/static/js/main.e6943cea.chunk.js"
+  },
+  {
+    "revision": "aa32ba7d4b6524ac13d0",
+    "url": "/ring-builder-react/static/js/1.aa32ba7d.chunk.js"
+  },
+  {
+    "revision": "f669b538671f70827b22",
+    "url": "/ring-builder-react/static/js/runtime~main.f669b538.js"
+  },
+  {
+    "revision": "6be9b5b3cca2ea718f0cbf4a400a5031",
+    "url": "/ring-builder-react/index.html"
+  }
+];
